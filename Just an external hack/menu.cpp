877dba6b94f835e15e7962f946f4f2d706bd0f77
Joshua Sutton <48036337+joshuatsutton@users.noreply.github.com>
2020-07-22 15:53:16 -0400

    Add project files.

---
+++ Just an external hack/menu.cpp
@@ -0,0 +1,60 @@
+#include "menu.hpp"
+
+static bool init = false;
+
+void ColorPicker(const char* name, float* color, bool alpha)
+{
+	ImGuiWindow* window = ImGui::GetCurrentWindow();
+	ImGuiStyle* style = &ImGui::GetStyle();
+
+	auto alphaSliderFlag = alpha ? ImGuiColorEditFlags_AlphaBar : ImGuiColorEditFlags_NoAlpha;
+
+	ImGui::SameLine(219.f);
+	ImGui::ColorEdit4(std::string{ "##" }.append(name).append("Picker").c_str(), color, alphaSliderFlag | ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip);
+
+}
+
+void initalize()
+{
+	ImGui::SetNextWindowSize(ImVec2(400, 300));
+	ImGui::SetNextWindowPos(ImVec2(0, 0));
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.WindowRounding = 0.f;
+
+	init = true;
+}
+
+void render()
+{
+	if (!init) initalize();
+
+	ImGui::Begin("Just an external hack", nullptr, windowFlags);
+
+	if (!foundCS)
+	{
+		if (ImGui::Button("Open google"))
+			ShellExecute(0, 0, L"https://apollohooks.wixsite.com/apollo", 0, 0, SW_SHOW);
+		ImGui::Text("Please open CS:GO");
+		ImGui::End();
+		return;
+	}
+
+	ImGui::Checkbox("Enemy glow", &enemyGlow);
+	ColorPicker("Enemy color", enemyColor, false);
+	ImGui::SliderFloat("Glow alpha", &enemyColor[3], 0.f, 1.f);
+	ImGui::Combo("Glow type", &enemyType, "Default\0Pulse\0Arms race");
+
+	ImGui::Checkbox("Team glow", &teamGlow);
+	ColorPicker("Team color", teamColor, false);
+	ImGui::SliderFloat("Glow alpha ", &teamColor[3], 0.f, 1.f);
+	ImGui::Combo("Glow type ", &teamType, "Default\0Pulse\0Arms race");
+
+	ImGui::Separator();
+
+	ImGui::Checkbox("Bunny hop", &doBhop);
+	ImGui::Checkbox("No flash", &noFlash);
+	ImGui::Checkbox("Reveal enemies on radar", &revealRadar);
+
+	ImGui::End();
+}
